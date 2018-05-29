01ad4fef1e5b8809f9b31f72ab4fa44ca94d5480
Malika Ihle <malika_ihle@hotmail.fr>
2018-05-29 17:19:16 -0400

    first commit

---
+++ Preregistration_part2.R
@@ -0,0 +1,8 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#	 Malika IHLE      malika_ihle@hotmail.fr
+#	 Preregistration FlexibleSexualAppetite part 2
+#  simulation of data to see whether planned analyses code works
+#	 Start : 29/05/2018
+#	 last modif : 
+#	 commit: start
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
