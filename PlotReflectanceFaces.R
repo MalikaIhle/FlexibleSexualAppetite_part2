367a8a535c322b1ea74a861c653142a7b444c557
Ihle <malika.ihle@ufl.edu>
2018-09-11 14:46:52 -0400

    reflectance male red faces (natural vs painted)

---
+++ PlotReflectanceFaces.R
@@ -0,0 +1,21 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#	 Malika IHLE      malika_ihle@hotmail.fr
+#	 Plot reflectance red faces: painted vs unmanipulated
+#	 Start : 09/11/2018
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+table <- read.table("Reflectance.txt", header=TRUE)
+head(table)
+
+library(ggplot2)
+
+
+  
+ggplot(data=table, aes(x=Wavelength, y=Reflectance, colour = Red, fill = Red)) +
+  scale_color_manual(values = c("Natural"="firebrick","Painted" ="red")) +
+  geom_line() +
+  geom_ribbon(aes(x = Wavelength, ymin=LowCI, ymax=UpperCI), linetype=1, alpha=0.2, color=NA) +
+  scale_fill_manual(values = c("Natural"="firebrick","Painted" ="red"), guide = FALSE)
+
+
+
