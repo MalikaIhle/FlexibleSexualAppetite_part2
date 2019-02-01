b511fadd97a2f57e20706d1f3a767e44af57db4d
Ihle <malika.ihle@ufl.edu>
2019-02-01 17:24:40 -0500

    check what videos has been watched already and those unwatched yet that we actually would need

--- VideoAnalyses.R
+++ VideoAnalyses.R
@@ -6,8 +6,7 @@
 #	 commit: get the descriptive comparison between unmanipulated and painted males for JoVE paper
 # !!!!! students need to correct courtships where FResp is empty
 # !!!!!!!! students need to correct wrong chronology in courtship time start and end !
-# ! should also check whether they updated timestopped watching if had initially ticked in progress (check if courtships or attacks occured after time stopped watching)
-# ! need to check for double copulations
+# # ! need to check for double copulations
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 {# Remarks
@@ -57,7 +56,7 @@ rm(list = ls(all = TRUE))
 
 {# load data
   
-conDB= odbcConnectAccess2007("VideoAnalyses_MaleTests_2018_BackEnd.accdb")
+conDB= odbcConnectAccess2007("RawData/VideoAnalyses_MaleTests_2018_BackEnd.accdb")
 sqlTables(conDB)
 
 Basic_Trials <- sqlFetch(conDB, 'Basic_Trials')
@@ -112,10 +111,19 @@ for (i in 1:nrow(TimeEND)){
 
 TimeEND$TimeEnd <- as.POSIXct(TimeEND$TimeEnd)
 
-# remove courtships watched after copulation (onyl few videos were watched longer initially)
+# remove courtships watched after copulation (only few videos were watched longer initially)
 Behav_Video_MaleTest <- merge(Behav_Video_MaleTest,TimeEND[,c('VideoID','TimeEnd')], all.x=TRUE)
 Behav_Male_Courtships <- merge(Behav_Male_Courtships,TimeEND[,c('VideoID','TimeEnd')], all.x=TRUE)
 TooLateCourtships <- Behav_Male_Courtships$CourtshipID[Behav_Male_Courtships$TimeEnd < Behav_Male_Courtships$CourtshipStart]
+      
+      # to check
+      # x <- merge(
+      #   Behav_Male_Courtships[Behav_Male_Courtships$CourtshipStart>Behav_Male_Courtships$TimeEnd,c('VideoID', 'CourtshipID','CourtshipStart','TimeEnd')],
+      #   Behav_Video_MaleTest[,c("VideoID", "FID")], all.x=TRUE)
+      # y <- merge(x, Behav_Female[,c("FID",'CopDuringVideo','EatDuringVideo')], all.x=TRUE)
+      # head(y)
+      # y[y$CopDuringVideo == 0 & y$EatDuringVideo==0,]
+
 Behav_Male_Courtships <- Behav_Male_Courtships[!Behav_Male_Courtships$CourtshipID%in%TooLateCourtships,]
 
 # calculate delays or durations
@@ -130,8 +138,6 @@ head(Behav_Video_MaleTest)
 head(Behav_Female)
 head(Behav_Female_Attacks)
 head(Behav_Male_Courtships)
-
-Behav_Male_Courtships[Behav_Male_Courtships$CourtshipStart>Behav_Male_Courtships$TimeEnd,c('VideoID', 'CourtshipID','CourtshipStart','TimeEnd')]
 
 {# summarize type of interactions
 
@@ -187,31 +193,31 @@ NaiveCourts
 
 {# Combine summaries
 
-MY_TABLE <- Behav_Video_MaleTest[,c('VideoID','FID','MID','DelayLeaveDish','TotalWatch','VideoTimeStart')]
-MY_TABLE <- arrange(MY_TABLE,VideoID)
+MY_TABLE_Videos <- Behav_Video_MaleTest[,c('VideoID','FID','MID','DelayLeaveDish','TotalWatch','VideoTimeStart')]
+MY_TABLE_Videos <- arrange(MY_TABLE_Videos,VideoID)
 
-MY_TABLE <- merge (x=MY_TABLE, y= FAttacks[,c('FirstFAttack', 'NbFAttacks', 'VideoID')],by='VideoID', all.x=TRUE)
-MY_TABLE <- merge (x=MY_TABLE, y= TotalIntendedFAttacks[,c('NbIntendedFAttacks', 'VideoID')],by='VideoID', all.x=TRUE)
-MY_TABLE <- merge (x=MY_TABLE, y= Cannibalism[,c('ConsumTime', 'VideoID')],by='VideoID', all.x=TRUE)
-MY_TABLE <- merge (x=MY_TABLE, y= AllCourts[,c('NBCourt','TotalCourtDur', 'FirstCourt','VideoID')],by='VideoID', all.x=TRUE)
-MY_TABLE <- merge (x=MY_TABLE, y= NaiveCourts[,c('NaiveNBCourt','NaiveTotalCourtDur', 'NaiveFirstCourt','VideoID')],by='VideoID', all.x=TRUE)
+MY_TABLE_Videos <- merge (x=MY_TABLE_Videos, y= FAttacks[,c('FirstFAttack', 'NbFAttacks', 'VideoID')],by='VideoID', all.x=TRUE)
+MY_TABLE_Videos <- merge (x=MY_TABLE_Videos, y= TotalIntendedFAttacks[,c('NbIntendedFAttacks', 'VideoID')],by='VideoID', all.x=TRUE)
+MY_TABLE_Videos <- merge (x=MY_TABLE_Videos, y= Cannibalism[,c('ConsumTime', 'VideoID')],by='VideoID', all.x=TRUE)
+MY_TABLE_Videos <- merge (x=MY_TABLE_Videos, y= AllCourts[,c('NBCourt','TotalCourtDur', 'FirstCourt','VideoID')],by='VideoID', all.x=TRUE)
+MY_TABLE_Videos <- merge (x=MY_TABLE_Videos, y= NaiveCourts[,c('NaiveNBCourt','NaiveTotalCourtDur', 'NaiveFirstCourt','VideoID')],by='VideoID', all.x=TRUE)
 
-summary(MY_TABLE)
-MY_TABLE <- MY_TABLE %>%
+summary(MY_TABLE_Videos)
+MY_TABLE_Videos <- MY_TABLE_Videos %>%
   mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .))) # changes delay to leave dish, NbAttacks, NbCourt
 
-MY_TABLE <- merge (x = MY_TABLE, y=  Behav_Female[,c('FID', 'CopulateYN', 'CopDuringVideo', 'CannibalizeYN', 'EatDuringVideo')],
+MY_TABLE_Videos <- merge (x = MY_TABLE_Videos, y=  Behav_Female[,c('FID','TrialDate', 'CopulateYN', 'CopDuringVideo', 'CannibalizeYN', 'EatDuringVideo')],
                           by = 'FID', all.x=TRUE)  
 
 ## Convert first times into delay from time start video, in seconds
 
-MY_TABLE$DelayFirstFAttack <- as.numeric(difftime(MY_TABLE$FirstFAttack,MY_TABLE$VideoTimeStart, units = 'secs'))
-MY_TABLE$DelayConsum <- as.numeric(difftime(MY_TABLE$ConsumTime,MY_TABLE$VideoTimeStart, units = 'secs'))
-MY_TABLE$DelayFirstCourt <- as.numeric(difftime(MY_TABLE$FirstCourt,MY_TABLE$VideoTimeStart, units = 'secs'))
-MY_TABLE$DelayNaiveFirstCourt <- as.numeric(difftime(MY_TABLE$NaiveFirstCourt,MY_TABLE$VideoTimeStart, units = 'secs'))
+MY_TABLE_Videos$DelayFirstFAttack <- as.numeric(difftime(MY_TABLE_Videos$FirstFAttack,MY_TABLE_Videos$VideoTimeStart, units = 'secs'))
+MY_TABLE_Videos$DelayConsum <- as.numeric(difftime(MY_TABLE_Videos$ConsumTime,MY_TABLE_Videos$VideoTimeStart, units = 'secs'))
+MY_TABLE_Videos$DelayFirstCourt <- as.numeric(difftime(MY_TABLE_Videos$FirstCourt,MY_TABLE_Videos$VideoTimeStart, units = 'secs'))
+MY_TABLE_Videos$DelayNaiveFirstCourt <- as.numeric(difftime(MY_TABLE_Videos$NaiveFirstCourt,MY_TABLE_Videos$VideoTimeStart, units = 'secs'))
 
 ### remove times
-MY_TABLE <- subset(MY_TABLE, select = - c(VideoTimeStart,FirstFAttack,ConsumTime,FirstCourt,NaiveFirstCourt))
+MY_TABLE_Videos <- subset(MY_TABLE_Videos, select = - c(VideoTimeStart,FirstFAttack,ConsumTime,FirstCourt,NaiveFirstCourt))
 
 ## add M and F Trt
 head(Basic_Trials)
@@ -221,45 +227,55 @@ colnames(MTRT) <- c('MID', 'MTrt')
 FTRT <- Basic_Trials[Basic_Trials$Sex == 0,c('Ind_ID','GroupName')]
 colnames(FTRT) <- c('FID', 'FTrt')
 
-MY_TABLE <- merge(MY_TABLE,MTRT, all.x=TRUE)
-MY_TABLE <- merge(MY_TABLE,FTRT, all.x=TRUE)
+MY_TABLE_Videos <- merge(MY_TABLE_Videos,MTRT, all.x=TRUE)
+MY_TABLE_Videos <- merge(MY_TABLE_Videos,FTRT, all.x=TRUE)
 
-summary(MY_TABLE)
+summary(MY_TABLE_Videos)
 
 }
 
-head(MY_TABLE)
+head(MY_TABLE_Videos)
 
 
 #~~~ ANALYSES
 
+## For JoVE paper, need to compare the 14 unmanipulated males to males tested at the same time 
+## (i.e. at the end of the season - which may be a specific subset of males)
+
+DatesTrailsUnmanipulated <- MY_TABLE_Videos$TrialDate[MY_TABLE_Videos$MTrt == "Unmanipulated"]
+summary((MY_TABLE_Videos[MY_TABLE_Videos$TrialDate > min(DatesTrailsUnmanipulated),]))
+summary(DatesTrailsUnmanipulated)
+Unmanip <- Basic_Trials$Ind_ID[Basic_Trials$GroupName == "Unmanipulated"]
+Unmanip[!Unmanip%in%MY_TABLE_Videos$MID[MY_TABLE_Videos$MTrt == "Unmanipulated"]]
+
+
 {## Preregistered
 ### comparison delay to court for both type of male in the valid tests (not excluded because one of the three spiders died for other reason than cannibalism)
 
-modDelayCourt <- lm(DelayFirstCourt ~ relevel(MTrt,ref= "Unmanipulated") , data = MY_TABLE)
+modDelayCourt <- lm(DelayFirstCourt ~ relevel(MTrt,ref= "Unmanipulated") , data = MY_TABLE_Videos)
 summary(modDelayCourt) # none of the painted categories are different from the unmanipulated. effect opposite expectation (unmanip take longer to court)
 }
 
 {## Exploratory
 ### are painted males behaving differently than unmanipulated males, or are they receiving more attacks from the female ?
 
-modDelayNaiveCourtAllVideo <- lm(DelayNaiveFirstCourt ~ relevel(MTrt,ref= "Unmanipulated"), data = MY_TABLE)
+modDelayNaiveCourtAllVideo <- lm(DelayNaiveFirstCourt ~ relevel(MTrt,ref= "Unmanipulated"), data = MY_TABLE_Videos)
 summary(modDelayNaiveCourtAllVideo)# 
 
 
-modDelayLeaveDish <- lm(DelayLeaveDish~ relevel(MTrt,ref= "Unmanipulated") ,data = MY_TABLE)
+modDelayLeaveDish <- lm(DelayLeaveDish~ relevel(MTrt,ref= "Unmanipulated") ,data = MY_TABLE_Videos)
 summary(modDelayLeaveDish)#
 
 
-modNbFAttacks <- lm(NbFAttacks~ relevel(MTrt,ref= "Unmanipulated") ,data = MY_TABLE)
+modNbFAttacks <- lm(NbFAttacks~ relevel(MTrt,ref= "Unmanipulated") ,data = MY_TABLE_Videos)
 summary(modNbFAttacks)#
 
  
-modTotalCourtDur <- lm(TotalCourtDur~ relevel(MTrt,ref= "Unmanipulated") ,data = MY_TABLE)
+modTotalCourtDur <- lm(TotalCourtDur~ relevel(MTrt,ref= "Unmanipulated") ,data = MY_TABLE_Videos)
 summary(modTotalCourtDur)#
 
 
-modNaiveTotalCourtDur <- lm(NaiveTotalCourtDur~relevel(MTrt,ref= "Unmanipulated"), data = MY_TABLE)
+modNaiveTotalCourtDur <- lm(NaiveTotalCourtDur~relevel(MTrt,ref= "Unmanipulated"), data = MY_TABLE_Videos)
 summary(modNaiveTotalCourtDur)# 
 
 }
@@ -267,34 +283,34 @@ summary(modNaiveTotalCourtDur)#
 {## Descriptive
 
 {### Nb of males who didn't court
-summary(MY_TABLE$NBCourt)
-length(MY_TABLE$NBCourt[MY_TABLE$NBCourt == 0])/
-  length(MY_TABLE$NBCourt)*100 # 
+summary(MY_TABLE_Videos$NBCourt)
+length(MY_TABLE_Videos$NBCourt[MY_TABLE_Videos$NBCourt == 0])/
+  length(MY_TABLE_Videos$NBCourt)*100 # 
 }
 
 {### Nb of males who didn't court BUT not because they were eaten 
-summary(MY_TABLE$NBCourt[MY_TABLE$EatDuringVideo == 1])
-length(MY_TABLE$NBCourt[MY_TABLE$EatDuringVideo == 1])/
-  length(MY_TABLE$NBCourt[MY_TABLE$EatDuringVideo == 0])*100 # 
-MY_TABLE$MTrt[MY_TABLE$NBCourt == 0 & MY_TABLE$EatDuringVideo == 0]
+summary(MY_TABLE_Videos$NBCourt[MY_TABLE_Videos$EatDuringVideo == 1])
+length(MY_TABLE_Videos$NBCourt[MY_TABLE_Videos$EatDuringVideo == 1])/
+  length(MY_TABLE_Videos$NBCourt[MY_TABLE_Videos$EatDuringVideo == 0])*100 # 
+MY_TABLE_Videos$MTrt[MY_TABLE_Videos$NBCourt == 0 & MY_TABLE_Videos$EatDuringVideo == 0]
 
 }
 
 {### Nb of videos where female attacked (or at least intended to)
-summary(MY_TABLE$NbIntendedFAttacks)
-length(MY_TABLE$NbIntendedFAttacks[MY_TABLE$NbIntendedFAttacks > 0])/
-  length(MY_TABLE$NbIntendedFAttacks)*100 #
+summary(MY_TABLE_Videos$NbIntendedFAttacks)
+length(MY_TABLE_Videos$NbIntendedFAttacks[MY_TABLE_Videos$NbIntendedFAttacks > 0])/
+  length(MY_TABLE_Videos$NbIntendedFAttacks)*100 #
 
 } 
 
 ### average time watched
-summary(MY_TABLE$TotalWatch)/60 # in min
+summary(MY_TABLE_Videos$TotalWatch)/60 # in min
 
 ### average delay to court
-summary(MY_TABLE$DelayFirstCourt)/60 # in min
+summary(MY_TABLE_Videos$DelayFirstCourt)/60 # in min
 
 ### average duration courting (out of duration watched)
-summary(60*(MY_TABLE$TotalCourtDur/60)/(MY_TABLE$TotalWatch/60)) # in min of coursthip per hour
+summary(60*(MY_TABLE_Videos$TotalCourtDur/60)/(MY_TABLE_Videos$TotalWatch/60)) # in min of coursthip per hour
 
 ### Nb of courtships that lead to female attack
 summary(Behav_Male_Courtships$FemaleResponse)
