ad96cf01cd0b7eff7de3150b56f5309671bfc970
Malika Ihle <malika_ihle@hotmail.fr>
2019-06-26 11:28:06 +0200

    querying data

--- 2_DataProcessing/DataExtraction.R
+++ 2_DataProcessing/DataExtraction.R
@@ -7,8 +7,12 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 {# Remarks
- 
-}
+# FID 18401 has no weight (had a typo in the weight of both (before after) that could not be corrected)
+# MID 18228 (with FID 18072) and MID 18390 (with FID 18478) have no weight
+# companion males were IDed but not weighted (only measured at maturity like all spiders)
+  
+  
+  }
 
 
 rm(list = ls(all = TRUE))
@@ -25,10 +29,10 @@ rm(list = ls(all = TRUE))
   
   Females <- sqlQuery(conDB, "
                       
-SELECT Basic_Individuals.Ind_ID AS FID, Basic_Trials.GroupName AS FTrt, Behav_Female.TrialDate, Behav_Female.CopulateYN, Behav_Female.CopDuringVideo, Behav_Female.CannibalizeYN, Behav_Female.EatDuringVideo, Behav_Female.CannibalismTime, Behav_Female.CannibalismDate, Behav_Female.Remarks AS TestRemarks
-FROM (Basic_Individuals LEFT JOIN Basic_Trials ON Basic_Individuals.Ind_ID = Basic_Trials.Ind_ID) LEFT JOIN Behav_Female ON Basic_Individuals.Ind_ID = Behav_Female.FID
-WHERE (((Basic_Individuals.Sex)=0) AND ((Basic_Trials.Experiment)='VirginMateChoice') AND ((Behav_Female.TestName)='Male'));
-                       
+SELECT Basic_Individuals.Ind_ID AS FID, Basic_Trials.GroupName AS FTrt, Behav_Female.TrialDate, Behav_Female.CopulateYN, Behav_Female.CopDuringVideo, Behav_Female.CannibalizeYN, Behav_Female.EatDuringVideo, Behav_Female.CannibalismTime, Behav_Female.CannibalismDate, Behav_Female.Remarks AS TestRemarks, Morph_Measurements.Mass AS FMass, Morph_Measurements_1.CarapaceWidth AS FCarapaceWidth
+FROM (((Basic_Individuals LEFT JOIN Basic_Trials ON Basic_Individuals.Ind_ID = Basic_Trials.Ind_ID) LEFT JOIN Behav_Female ON Basic_Individuals.Ind_ID = Behav_Female.FID) LEFT JOIN Morph_Measurements ON Behav_Female.FID = Morph_Measurements.Ind_ID) LEFT JOIN Morph_Measurements AS Morph_Measurements_1 ON Behav_Female.FID = Morph_Measurements_1.Ind_ID
+                      WHERE (((Basic_Individuals.Sex)=0) AND ((Basic_Trials.Experiment)='VirginMateChoice') AND ((Behav_Female.TestName)='Male') AND ((Morph_Measurements.Occasion)='VirginMateChoice') AND ((Morph_Measurements_1.Occasion)='maturity'));
+                      
                       ")
   
   summary(Females)
@@ -44,6 +48,31 @@ Males <- sqlQuery(conDB, "
 
 summary(Males)
 nrow(Males)
+
+
+PaintedMalesMeasurements <- sqlQuery(conDB, "
+
+SELECT Mwidth.MID, Mwidth.MCarapaceWidth, MMass.Mass
+FROM (
+SELECT Basic_Individuals.Ind_ID AS MID, Morph_Measurements.CarapaceWidth AS MCarapaceWidth
+                                     FROM (Basic_Individuals LEFT JOIN Basic_Trials ON Basic_Individuals.Ind_ID = Basic_Trials.Ind_ID) LEFT JOIN Morph_Measurements ON Basic_Trials.Ind_ID = Morph_Measurements.Ind_ID
+                                     WHERE (((Basic_Trials.GroupName)<>'Companion') AND ((Basic_Trials.GroupNumber) Is Not Null) AND ((Basic_Individuals.Sex)=1) AND ((Basic_Trials.Experiment)='VirginMateChoice') AND ((Morph_Measurements.Occasion)='maturity'))
+) AS Mwidth 
+
+LEFT JOIN (
+SELECT Basic_Trials.Ind_ID AS MID, Morph_Measurements.Mass
+                                     FROM (Basic_Individuals LEFT JOIN Basic_Trials ON Basic_Individuals.Ind_ID = Basic_Trials.Ind_ID) LEFT JOIN Morph_Measurements ON Basic_Trials.Ind_ID = Morph_Measurements.Ind_ID
+                                     WHERE (((Basic_Trials.GroupName)<>'Companion') AND ((Basic_Individuals.Sex)=1) AND ((Basic_Trials.Experiment)='VirginMateChoice') AND ((Morph_Measurements.Occasion)='VirginMateChoice'))
+) AS MMass 
+
+ON Mwidth.MID = MMass.MID;
+
+  ")
+
+
+summary(PaintedMalesMeasurements)
+nrow(PaintedMalesMeasurements)
+
 
 Fitness <- sqlQuery(conDB, "
 
